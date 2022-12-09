6be0292b5e0540880e4b338a06978ca9b3c878d9
Andrei Chupeev <cad.dsw.dwa@gmail.com>
2022-12-09 22:02:34 +0500

    add lab_6

---
+++ Lab_6/Task_2/Task_2/HashTableBuilder.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Task_2
+{
+    public class HashTableBuilder
+    {
+        public static void CreateHashTableBuilder(int N)
+        {
+            var r = new Random();
+            var numbers = new List<int>();
+            var hashList = new List<int>[N/100];
+            var minHashList = N + 1;
+            var maxHashList = -1;
+            for (var i = 0; i<N;i++)
+            {
+                var number = r.Next(1, 4 * N);
+                if (numbers.Contains(number)) continue;
+                numbers.Add(number);
+                var hash = GetHash(number,N);
+                if (hashList[hash] == null)
+                {
+                    hashList[hash] = new List<int>();
+                    hashList[hash].Add(number);
+                }
+                else
+                {
+                    hashList[hash].Add(number);
+                }
+                maxHashList = Math.Max(hashList[hash].Count, maxHashList);
+            }
+            
+            Console.WriteLine($"Максимальная длина при N = {N} - {maxHashList}");
+            Console.WriteLine($"Минимальная длина при N = {N} - {hashList.Min(x => x.Count)}");
+        }
+
+        private static int GetHash(int n, int N)
+        {
+            return (n + 23) % (N/100);
+        }
+    }
+}
---
+++ Lab_6/Task_2/Task_2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Task_2
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            HashTableBuilder.CreateHashTableBuilder(5000);
+            Console.WriteLine();
+            HashTableBuilder.CreateHashTableBuilder(10000);
+            Console.WriteLine();
+            HashTableBuilder.CreateHashTableBuilder(20000);
+        }
+    }
+}
